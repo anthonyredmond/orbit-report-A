6566e034452b46fb85b4fa40d496ad71d4d7e401
repl.it user <replituser@example.com>
2020-12-30 02:55:14 +0000

    added shouldShowWarning property definition

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,6 +4,7 @@ export class Satellite {
     type: string;
     operational: boolean;
     launchDate: string;
+    showShowWarning: boolean;
     
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
       this.name = name;
